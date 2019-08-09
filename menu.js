fa377f119642a86c809ef8ef6597d2ec43c2edda
Andre Mougeot <mougeota@fcc.ca>
2019-08-09 16:34:56 -0600

    Separate clean UI and fullscreen mode functions

--- menu.js
+++ menu.js
@@ -1,10 +1,16 @@
-let dashboardModeButton = document.getElementById('dashboardModeButton');
-
-dashboardModeButton.onclick = function(element) {
+document.getElementById('fullscreenButton').onclick = function() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.executeScript(
         tabs[0].id,
-        {file: 'enableDashboardMode.js'});
+        {file: 'fullscreenMode.js'});
     window.close();
+  });
+};
+
+document.getElementById('cleanUIButton').onclick = function() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+        tabs[0].id,
+        {file: 'cleanUI.js'});
   });
 };
